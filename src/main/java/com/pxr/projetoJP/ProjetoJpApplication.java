ddb9e122127d43d5334bedeeaa7be29e40e00419
LisandroPeixer <lisandropeixer@yahoo.com.br>
2019-11-08 11:17:04 -0300

    Projeto criado

---
+++ src/main/java/com/pxr/projetoJP/ProjetoJpApplication.java
@@ -0,0 +1,13 @@
+package com.pxr.projetoJP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pxr/projetoJP/ProjetoJpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxr.projetoJP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
