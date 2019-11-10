d1df4d6cdb34b2a2378cde9021b1d4a02a8740d4
LisandroPeixer <lisandropeixer@yahoo.com.br>
2019-11-09 21:21:12 -0300

    Sistema de pedidos implementado

--- src/main/java/com/pxr/projetoJP/ProjetoJpApplication.java
+++ src/main/java/com/pxr/projetoJP/ProjetoJpApplication.java
@@ -1,5 +1,6 @@
 package com.pxr.projetoJP;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,22 @@ import com.pxr.projetoJP.domain.Cidade;
 import com.pxr.projetoJP.domain.Cliente;
 import com.pxr.projetoJP.domain.Endereco;
 import com.pxr.projetoJP.domain.Estado;
+import com.pxr.projetoJP.domain.ItemPedido;
+import com.pxr.projetoJP.domain.Pagamento;
+import com.pxr.projetoJP.domain.PagamentoComBoleto;
+import com.pxr.projetoJP.domain.PagamentoComCartao;
+import com.pxr.projetoJP.domain.Pedido;
 import com.pxr.projetoJP.domain.Produto;
+import com.pxr.projetoJP.domain.enums.EstadoPagamento;
 import com.pxr.projetoJP.domain.enums.TipoCliente;
 import com.pxr.projetoJP.repositories.CategoriaRepository;
 import com.pxr.projetoJP.repositories.CidadeRepository;
 import com.pxr.projetoJP.repositories.ClienteRepository;
 import com.pxr.projetoJP.repositories.EnderecoRepository;
 import com.pxr.projetoJP.repositories.EstadoRepository;
+import com.pxr.projetoJP.repositories.ItemPedidoRepository;
+import com.pxr.projetoJP.repositories.PagamentoRepository;
+import com.pxr.projetoJP.repositories.PedidoRepository;
 import com.pxr.projetoJP.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -38,6 +48,14 @@ public class ProjetoJpApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 		
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoJpApplication.class, args);
@@ -116,5 +134,41 @@ public class ProjetoJpApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli2, e2);
+		Pedido ped3 = new Pedido(null, sdf.parse("11/10/2017 23:35"), cli2, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.CANCELADO, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		Pagamento pagto3 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped3, sdf.parse("21/10/2017 00:00"), null);
+		ped3.setPagamento(pagto3);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1));
+		cli2.getPedidos().addAll(Arrays.asList(ped2, ped3));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2, ped3));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2, pagto3));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p3, 60.00, 10, 540.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p5, 90.00, 30, 810.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p1, 0.00, 3, 210.00);
+		ItemPedido ip4 = new ItemPedido(ped3, p1, 70.00, 10, 630.00);
+
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		ped3.getItens().addAll(Arrays.asList(ip4));
+
+		p1.getItens().addAll(Arrays.asList(ip3, ip4));
+		p3.getItens().addAll(Arrays.asList(ip1));
+		p5.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3, ip4));
+		
 	}
 } 
--- src/main/java/com/pxr/projetoJP/domain/Cliente.java
+++ src/main/java/com/pxr/projetoJP/domain/Cliente.java
@@ -14,7 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pxr.projetoJP.domain.enums.TipoCliente;
 
 @Entity
@@ -29,13 +29,16 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 	}
@@ -105,6 +108,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
