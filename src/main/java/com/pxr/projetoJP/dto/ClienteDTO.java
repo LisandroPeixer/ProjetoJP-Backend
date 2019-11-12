f1f822953957d20defe7387b2bd5f26ea1db7b55
LisandroPeixer <lisandropeixer@yahoo.com.br>
2019-11-12 12:13:11 -0300

    Validações de CPF CNPJ e email

--- src/main/java/com/pxr/projetoJP/dto/ClienteDTO.java
+++ src/main/java/com/pxr/projetoJP/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.pxr.projetoJP.domain.Cliente;
+import com.pxr.projetoJP.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
